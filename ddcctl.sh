33f0c6b5135fb8ab3014acb469bbf9f7d0f71bf5
samarth <samarthiskhare@gmail.com>
2020-11-22 15:33:11 -0800

    additional fixes

--- ddcctl.sh
+++ ddcctl.sh
@@ -4,13 +4,14 @@
 #dell="./ddcctl -d 1"
 #len="./ddcctl -d 2"
 # TODO : Automate night and day figures based on sunrise and sunset time https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400
+#TODO: Enable boolean toggle on/off for settings
 
 dim() {
 	./ddcctl -d 1 -b 10 -c 10
 }
 
 bright() {
-	./ddcctl -d 1 -b 50 -c 50
+	./ddcctl -d 1 -b 40 -c 40
 }
 
 up() {
@@ -21,14 +22,28 @@ down() {
 	./ddcctl -d 1 -b 20- -c 12-
 }
 
-case "$1" in
-	dim|bright|up|down) $1;;
-	*)	#no scheme given, match local Hour of Day
-		#HoD=$(date +%k) #hour of day
-                HM=$(date +"%H:%M")
-                HoD=$(echo $HM) | cut -d':' -f1
-                MoH=$(echo $HM) | cut -d':' -f2
-		let "night = (( $HoD < 7 || $HoD > 18 ))" #daytime is 7a-7p
-		(($night)) && dim || bright
-		;;
-esac
+full() {
+        ./ddcctl -d 1 -b 50 -c 50
+}
+
+DAY=6
+MID=12
+NIGHT=18
+
+HM=$(date +"%H:%M")
+HoD=$(echo $(echo $HM) | cut -d':' -f1)
+MoH=$(echo $(echo $HM) | cut -d':' -f2) 
+
+echo $HoD
+if [ $HoD -lt $DAY ]
+then
+   dim
+elif [ $HoD -ge $DAY ] && [ $HoD -lt $MID ]
+then
+   full
+elif [ $HoD -ge $MID ]  && [ $HoD -lt $NIGHT ]
+then
+   bright
+else
+   dim
+fi
