771c6cb4b137f34da6edade9a5c097c0996f3e99
samarth <samarthiskhare@gmail.com>
2020-11-22 12:24:39 -0800

    added custom settings

--- ddcctl.sh
+++ ddcctl.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 #tweak OSX display monitors' brightness to a given scheme, increment, or based on the current local time
 
-hp="ddcctl -d 1"
-len="ddcctl -d 2"
+#dell="./ddcctl -d 1"
+#len="./ddcctl -d 2"
+# TODO : Automate night and day figures based on sunrise and sunset time https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400
 
 dim() {
-	$hp -b 42 -c 26
-	$len -b 4 -c 9
+	./ddcctl -d 1 -b 10 -c 10
 }
 
 bright() {
-	$hp -b 100 -c 75
-	$len -b 85 -c 80
+	./ddcctl -d 1 -b 50 -c 50
 }
 
 up() {
-	$hp -b 20+ -c 12+
-	$len -b 15+ -c 12+
+	./ddcctl -d 1 -b 20+ -c 12+
 }
 
 down() {
-	$hp -b 20- -c 12-
-	$len -b 15- -c 12-
+	./ddcctl -d 1 -b 20- -c 12-
 }
 
 case "$1" in
 	dim|bright|up|down) $1;;
 	*)	#no scheme given, match local Hour of Day
-		HoD=$(date +%k) #hour of day
+		#HoD=$(date +%k) #hour of day
+                HM=$(date +"%H:%M")
+                HoD=$(echo $HM) | cut -d':' -f1
+                MoH=$(echo $HM) | cut -d':' -f2
 		let "night = (( $HoD < 7 || $HoD > 18 ))" #daytime is 7a-7p
 		(($night)) && dim || bright
 		;;
